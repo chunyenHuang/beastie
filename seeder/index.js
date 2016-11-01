3187acfa27dbf2cf9204de3b5a8e1314b80dae78
John Huang <little78926@gmail.com>
2016-10-31 21:39:15 -0700

    Update index.js

--- seeder/index.js
+++ seeder/index.js
@@ -11,34 +11,45 @@ const dbUrl = process.env.MONGODB_URI || process.env.MONGOLAB_URI;
 
 // Load Datas
 const initDatas = {};
+const routes = [];
 const routePath = path.join(__dirname, '/seeds');
 fs.readdirSync(routePath).forEach((file) => {
     const filename = file.split('.')[0];
     const route = path.join(routePath, file);
     initDatas[filename] = require(route);
+    routes.push(filename);
 });
+console.log(routes);
 
-for (var prop in initDatas) {
-    const collection = prop;
-    dbClient.connect(dbUrl, (err, db) => {
+dbClient.connect(dbUrl, (err, db) => {
+    (function insert(index){
+        if(index==routes.length){
+            return db.close();
+        }
+        const collection = routes[index];
+        console.log(collection)
+        var count=0;
         db.collection(collection).remove({}, (err) => {
             if (!err) {
                 initDatas[collection].forEach((data) => {
                     if (data._id) {
                         data._id = ObjectId(data._id);
                     }
-                    db.collection(collection).insert(data, (err) => {
-                        if(!err){
-                            db.close();
-                            console.log('Init Data is inserted into ' + collection);
-                        } else {
+                    db.collection(collection).update(data,data,{upsert:true}, (err) => {
+                        count++;
+                        if(err){
                             console.log(err);
                         }
+                        
+                        if(count == initDatas[collection].length){
+                            index++
+                            insert(index);
+                        }              
                     });
                 });
             } else {
                 console.log(err);
             }
         });
-    });
-}
+    })(0);
+});
