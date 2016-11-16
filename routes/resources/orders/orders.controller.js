e3da7b7089693647bd53c47d4fce7ba8f3cc6f71
John Huang <little78926@gmail.com>
2016-11-15 23:17:31 -0800

    add order query

--- routes/resources/orders/orders.controller.js
+++ routes/resources/orders/orders.controller.js
@@ -28,6 +28,59 @@ class OrdersController extends AbstractController {
         };
         res.json(template);
     }
+
+    transformDates(req, res, next) {
+        if (req.body) {
+            for (let prop in req.body) {
+                if (
+                    prop == 'scheduleAt' ||
+                    prop == 'checkInAt' ||
+                    prop == 'updatedAt'
+                ) {
+                    if (typeof (req.body[prop]) == 'string') {
+                        console.log(new Date(req.body[prop]));
+                        req.body[prop] = new Date(req.body[prop]);
+                    }
+                }
+            }
+        }
+        next();
+    }
+
+    getByDate(req, res) {
+        // const yesterday = new Date(new Date().getTime() - 24 * 60 * 60 * 1000);
+        // const tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+
+        const past = new Date(new Date().getTime() - 1 * 24 * 60 * 60 * 1000);
+        const today = new Date();
+        const from = (req.query.from) ? req.query.from : past;
+        const to = (req.query.to) ? req.query.to : today;
+
+        const query = req.collection.find({
+            isDeleted: false,
+            scheduleAt: {
+                $gte: new Date(from),
+                $lt: new Date(to)
+            }
+        });
+        query.toArray((err, results) => {
+            // fix wrong condition
+            if (!err) {
+                if (results.length > 0) {
+                    res.statusCode = 200;
+                    res.json(results);
+                } else {
+                    // res.sendStatus(404);
+                    res.status(404).send({
+                        error: 'Sorry, we cannot find that!'
+                    });
+                }
+            } else {
+                res.sendStatus(500);
+            }
+        });
+
+    }
 }
 
 module.exports = OrdersController;
--- routes/resources/orders/orders.routes.js
+++ routes/resources/orders/orders.routes.js
@@ -3,23 +3,34 @@ const Orders = new OrdersController;
 const dbCollectionName = 'orders';
 
 module.exports = (app) => {
-    app.route('/orders?')
+    app.route('/ordersByDate?')
         .all((req, res, next) => {
             req.collection = req.db.collection(dbCollectionName);
             next();
         })
+        .get(Orders.getByDate);
+
+    app.route('/orders?')
+        .all(
+            Orders.transformDates,
+            (req, res, next) => {
+                req.collection = req.db.collection(dbCollectionName);
+                next();
+            })
         .get(Orders.query)
         .post(Orders.post);
 
     app.route('/orders?/:id')
-        .all((req, res, next) => {
-            if(req.params.id == 'template'){
-                Orders.getTemplate(req, res);
-            } else {
-                req.collection = req.db.collection(dbCollectionName);
-                next();
-            }
-        })
+        .all(
+            Orders.transformDates,
+            (req, res, next) => {
+                if (req.params.id == 'template') {
+                    Orders.getTemplate(req, res);
+                } else {
+                    req.collection = req.db.collection(dbCollectionName);
+                    next();
+                }
+            })
         .get(Orders.get)
         .put(Orders.put)
         .delete(Orders.delete);
