7b9c5a78bb8c1a60d909344dbd4428dd083890b8
John Huang <little78926@gmail.com>
2016-12-03 18:09:06 -0800

    add transactions module

--- client/src/core/services/index.js
+++ client/src/core/services/index.js
@@ -1,12 +1,10 @@
 import snapshotService from './snapshot.service';
 import TaskService from './task.service';
-import TransactionsService from './transactions.service';
 
 const coreServices = angular
     .module('beastie.core.services', [])
     .service('Snapshot', snapshotService)
     .service('Task', TaskService)
-    .service('Transactions', TransactionsService)
     .name;
 
 export default coreServices;
