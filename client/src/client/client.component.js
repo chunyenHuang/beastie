8888ad2a85817856095f5debedad2574f6f19fae
John Huang <little78926@gmail.com>
2016-12-18 01:12:49 -0800

    remove dev codes for productions

--- client/src/client/client.component.js
+++ client/src/client/client.component.js
@@ -52,7 +52,7 @@ const clientComponent = {
         }
 
         $onInit() {
-            // this.$state.go('client.customersCheckIn');
+            this.$state.go('client.customersCheckIn');
             // this.$state.go('client.signature');
         }
     }
