3345e381d6e6d86f1ad1f86cfa57441a79ef2211
John Huang <little78926@gmail.com>
2017-01-22 12:15:08 -0800

    update printer.js

--- routes/resources/printer/printer.controller.js
+++ routes/resources/printer/printer.controller.js
@@ -1,40 +1,41 @@
 const path = require('path');
-// const fs = require('fs');
+const escpos = require('escpos');
+const device = new escpos.USB();
+const printer = new escpos.Printer(device);
 
 const AbstractController = require('../../abstract/AbstractController.js');
 class PrinterController extends AbstractController {
     constructor(){
         super();
-        try {
-            const escpos = require('escpos');
-            this.escpos = escpos;
-            this.device = new escpos.USB();
-            this.printer = new escpos.Printer(this.device);
-        } catch (err) {
-            console.log('------------ Please Connect the Receipt Printer.');
-        }
-
     }
     check(req, res) {
-        if(
-            !this.device ||
-            !this.printer
-        ) {
+        if(!device){
+            console.log('No USB devices.');
             res.statusCode = 500;
             res.json({
-                message: 'Printer is not connected.'
+                message: 'No USB devices.'
             });
             return;
         }
-        if(!this.device.open){
+        if(!printer) {
+            console.log('No Printer devices.');
             res.statusCode = 500;
             res.json({
-                message: 'Printer is not connected.'
+                message: 'No Printer devices.'
             });
             return;
         }
-        this.device.open((err) => {
+        if(!device.open){
+            console.log('Can not open device.');
+            res.statusCode = 500;
+            res.json({
+                message: 'Can not open device.'
+            });
+            return;
+        }
+        device.open((err) => {
             if(err){
+                console.log(err);
                 res.statusCode = 500;
                 res.json({
                     message: 'Printer has error. Please reboot.'
@@ -47,65 +48,32 @@ class PrinterController extends AbstractController {
             }
         });
     }
-    test(req, res) {
-        if(!this.device){
-            res.statusCode = 500;
-            res.json({
-                message: 'Printer is not connected.'
-            });
-        } else {
-            // this.device.open(() => {
-            //     const today = new Date();
-            //     this.printer
-            //         .font('A')
-            //         .align('ct')
-            //         .style('b')
-            //         .size(2, 4)
-            //         .text(' ')
-            //         .text('Welcome to Beastie')
-            //         .text('歡迎使用比司吉寵物系統', 'Big5')
-            //         .text('------------------------')
-            //         // .raster(image)
-            //         .text('------------------------')
-            //         .text(today.toLocaleDateString())
-            //         .text(today.toLocaleTimeString())
-            //         .text(' ')
-            //         .text(' ')
-            //         .text(' ')
-            //         .text(' ')
-            //         .text(' ')
-            //         .cut();
-            //     res.sendStatus(200);
-            // });
 
-            const logoPath = path.join(global.root, 'logos/B1_256-01.png');
-            console.log(logoPath);
-            this.escpos.Image.load(logoPath, (image) => {
-                this.device.open(() => {
-                    const today = new Date();
-                    this.printer
-                        .font('A')
-                        .align('ct')
-                        .style('b')
-                        .size(2, 4)
-                        .text(' ')
-                        .text('Welcome to Beastie')
-                        .text('歡迎使用比司吉寵物系統', 'Big5')
-                        .text('------------------------')
-                        .raster(image)
-                        .text('------------------------')
-                        .text(today.toLocaleDateString())
-                        .text(today.toLocaleTimeString())
-                        .text(' ')
-                        .text(' ')
-                        .text(' ')
-                        .text(' ')
-                        .text(' ')
-                        .cut();
-                    res.sendStatus(200);
-                });
-            });
-        }
+    test(req, res) {
+        device.open(() => {
+            const today = new Date();
+            printer
+                .font('A')
+                .align('ct')
+                .style('b')
+                .size(2, 4)
+                .text(' ')
+                .text('Welcome to Beastie')
+                .text('歡迎使用比司吉寵物系統', 'Big5')
+                .text('------------------------')
+                // .raster(image)
+                .text('------------------------')
+                .text(today.toLocaleDateString())
+                .text(today.toLocaleTimeString())
+                .text(' ')
+                .text(' ')
+                .text(' ')
+                .text(' ')
+                .text(' ')
+                .cut();
+            console.log('printing test prints.');
+            res.sendStatus(200);
+        });
     }
 
 }
