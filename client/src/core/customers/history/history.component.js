af89c7944e8a0d858617a9a4a14935ad2b1662f5
John Huang <little78926@gmail.com>
2017-02-24 21:12:38 -0800

    show service and addons in transaction history

--- client/src/core/customers/history/history.component.js
+++ client/src/core/customers/history/history.component.js
@@ -14,7 +14,8 @@ const historyComponent = {
                 '$scope',
                 '$state',
                 '$stateParams',
-                'Orders'
+                'Orders',
+                'Transactions'
             ];
         }
         constructor(
@@ -23,7 +24,8 @@ const historyComponent = {
             $scope,
             $state,
             $stateParams,
-            Orders
+            Orders,
+            Transactions
         ) {
             this.$log = $log;
             this.$timeout = $timeout;
@@ -31,16 +33,33 @@ const historyComponent = {
             this.$state = $state;
             this.$stateParams = $stateParams;
             this.Orders = Orders;
+            this.Transactions = Transactions;
         }
-        $onChanges(){
-            if(this.customerId){
-                this.Orders.query({
+        $onChanges() {
+            if (this.customerId) {
+                this.history = [];
+                this.orders = [];
+                this.Transactions.query({
                     customer_id: this.customerId
-                }).$promise.then((res)=>{
-                    console.log(res);
-                    this.history = res;
-                });
+                }).$promise.then((transactions) => {
+                    this.history = transactions;
+                    transactions.forEach((tran) => {
+                        if (tran.order_id) {
+                            this.Orders.get({
+                                id: tran.order_id
+                            }).$promise.then((order) => {
+                                this.orders.push(order);
+                            }, () => {});
+                        }
+                    });
+                }, () => {});
             }
+        }
+
+        findOrder(transaction) {
+            return this.orders.find((order) => {
+                return (order._id === transaction.order_id);
+            });
         }
     }
 };
